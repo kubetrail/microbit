60e05f7068fec280bfda9b5a60501cf158f2fac1
Saurabh Deoras <sdeoras@gmail.com>
2025-03-06 07:04:14 -0800

    added temperature readout (#3)
    Signed-off-by: Saurabh Deoras <sdeoras@gmail.com>

--- microbit-v2.go
+++ microbit-v2.go
@@ -14,6 +14,147 @@ const (
 	NumCols = 5
 )
 
+type Display [NumRows][NumCols]uint8
+
+var (
+	DisplayZeros = [NumRows][NumCols]uint8{
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+	}
+
+	DisplayHeart = [NumRows][NumCols]uint8{
+		{0, 1, 0, 1, 0},
+		{1, 0, 1, 0, 1},
+		{1, 0, 0, 0, 1},
+		{0, 1, 0, 1, 0},
+		{0, 0, 1, 0, 0},
+	}
+
+	DisplayOnes = [NumRows][NumCols]uint8{
+		{1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1},
+	}
+
+	DisplaySquare = [NumRows][NumCols]uint8{
+		{1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 1},
+		{1, 0, 0, 0, 1},
+		{1, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1},
+	}
+
+	Display0 = [NumRows][NumCols]uint8{
+		{0, 0, 1, 0, 0},
+		{0, 1, 0, 1, 0},
+		{0, 1, 0, 1, 0},
+		{0, 1, 0, 1, 0},
+		{0, 0, 1, 0, 0},
+	}
+	Display1 = [NumRows][NumCols]uint8{
+		{0, 0, 1, 0, 0},
+		{0, 0, 1, 0, 0},
+		{0, 0, 1, 0, 0},
+		{0, 0, 1, 0, 0},
+		{0, 0, 1, 0, 0},
+	}
+	Display2 = [NumRows][NumCols]uint8{
+		{0, 1, 1, 1, 0},
+		{0, 1, 0, 1, 0},
+		{0, 0, 1, 0, 0},
+		{0, 1, 0, 0, 0},
+		{0, 1, 1, 1, 0},
+	}
+	Display3 = [NumRows][NumCols]uint8{
+		{0, 1, 1, 1, 0},
+		{0, 0, 0, 1, 0},
+		{0, 0, 1, 1, 0},
+		{0, 0, 0, 1, 0},
+		{0, 1, 1, 1, 0},
+	}
+	Display4 = [NumRows][NumCols]uint8{
+		{0, 1, 0, 1, 0},
+		{0, 1, 0, 1, 0},
+		{0, 1, 1, 1, 0},
+		{0, 0, 0, 1, 0},
+		{0, 0, 0, 1, 0},
+	}
+	Display5 = [NumRows][NumCols]uint8{
+		{0, 1, 1, 1, 0},
+		{0, 1, 0, 0, 0},
+		{0, 1, 1, 1, 0},
+		{0, 0, 0, 1, 0},
+		{0, 1, 1, 1, 0},
+	}
+	Display6 = [NumRows][NumCols]uint8{
+		{0, 1, 1, 1, 0},
+		{0, 1, 0, 0, 0},
+		{0, 1, 1, 1, 0},
+		{0, 1, 0, 1, 0},
+		{0, 1, 1, 1, 0},
+	}
+	Display7 = [NumRows][NumCols]uint8{
+		{0, 1, 1, 1, 0},
+		{0, 1, 0, 1, 0},
+		{0, 0, 1, 0, 0},
+		{0, 0, 1, 0, 0},
+		{0, 0, 1, 0, 0},
+	}
+	Display8 = [NumRows][NumCols]uint8{
+		{0, 1, 1, 1, 0},
+		{0, 1, 0, 1, 0},
+		{0, 0, 1, 0, 0},
+		{0, 1, 0, 1, 0},
+		{0, 1, 1, 1, 0},
+	}
+	Display9 = [NumRows][NumCols]uint8{
+		{0, 1, 1, 1, 0},
+		{0, 1, 0, 1, 0},
+		{0, 1, 1, 1, 0},
+		{0, 0, 0, 1, 0},
+		{0, 1, 1, 1, 0},
+	}
+	DisplayDegreeC = [NumRows][NumCols]uint8{
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 1, 0},
+		{1, 1, 0, 0, 0},
+		{1, 0, 0, 0, 0},
+		{1, 1, 0, 0, 0},
+	}
+	DisplayLeftArrow = [NumRows][NumCols]uint8{
+		{0, 0, 0, 0, 0},
+		{0, 1, 0, 0, 0},
+		{1, 0, 1, 1, 1},
+		{0, 1, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+	}
+	DisplayRightArrow = [NumRows][NumCols]uint8{
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 1, 0},
+		{1, 1, 1, 0, 1},
+		{0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0},
+	}
+)
+
+var Num2Display = map[int]Display{
+	0: Display0,
+	1: Display1,
+	2: Display2,
+	3: Display3,
+	4: Display4,
+	5: Display5,
+	6: Display6,
+	7: Display7,
+	8: Display8,
+	9: Display9,
+}
+
 type Device struct {
 	mu sync.Mutex
 
@@ -25,6 +166,8 @@ type Device struct {
 
 	buttonA machine.Pin
 	buttonB machine.Pin
+
+	capTouch machine.Pin
 }
 
 // NewDevice returns a new device
@@ -60,6 +203,8 @@ func NewDevice() *Device {
 	device.buttonA.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
 	device.buttonB.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
 
+	machine.InitADC()
+
 	return device
 }
 
@@ -68,6 +213,7 @@ func (device *Device) Wait(ctx context.Context) *Device {
 	select {
 	case <-ctx.Done():
 	}
+	time.Sleep(time.Millisecond * 500)
 
 	return device
 }
@@ -174,6 +320,39 @@ func (device *Device) Buzz(ctx context.Context, frequency uint32) *Device {
 		pin.Low()
 	}()
 
+	return device
+}
+
+func (device *Device) DisplayTemp(ctx context.Context) *Device {
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				rawTemp := machine.ReadTemperature()
+				temp := int(rawTemp) / 1000
+
+				digits := make([]int, 10)
+				count := 0
+				for i := 0; int(temp) > 0 && i < 10; i++ {
+					r := int(temp) % 10
+					temp /= 10
+					digits[i] = r
+					count++
+				}
+
+				for i := count - 1; i >= 0; i-- {
+					ctx2, cancel := context.WithDeadline(ctx, time.Now().Add(time.Second*1))
+					device.SetMatrix(Num2Display[digits[i]]).Display(ctx2).Wait(ctx2)
+					cancel()
+				}
+				ctx2, cancel := context.WithDeadline(ctx, time.Now().Add(time.Second*1))
+				device.SetMatrix(DisplayDegreeC).Display(ctx2).Wait(ctx2)
+				cancel()
+			}
+		}
+	}()
 	return device
 }
 
